56c713c8191372479e8e0384187b0a23d2b98fab
Sebastian Velandia <jsvelandiab@uqvirtual.edu.co>
2025-03-25 21:35:02 -0500

    Modificada Main

--- src/com/laboratorio/Main.java
+++ src/com/laboratorio/Main.java
@@ -1,9 +1,32 @@
 package com.laboratorio;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Bienvenidos al laboratorio de Git con Java");
+		public static void main(String[] args) {
+	        Scanner teclado = new Scanner(System.in);
+	        int numeroAleatorio = (int) (Math.random() * 100) + 1;
+	        int intentos = 0;
+	        int numeroIngresado = 0;
+
+	        System.out.println("Intenta adivinar un número entre 1 y 100.");
+
+	        while (numeroIngresado != numeroAleatorio) {
+	            System.out.print("Ingrese su número: ");
+	            numeroIngresado = teclado.nextInt();
+	            intentos++;
+
+	            if (numeroIngresado > numeroAleatorio) {
+	                System.out.println("El número es menor. Intenta de nuevo.");
+	            } else if (numeroIngresado < numeroAleatorio) {
+	                System.out.println("El número es mayor. Intenta de nuevo.");
+	            } else {
+	                System.out.println("¡Felicidades! El número era " + numeroAleatorio + " y lo adivinaste en " + intentos + " intentos.");
+	            }
+	        }
+
+	        teclado.close();
+	    }
 	}
 
-}
